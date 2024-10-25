3822eae93e0c12fd3df909d576948e0d8658daff
Vafelka3 <n.kondrashov6677@gmail.com>
2024-10-25 03:31:29 +0200

    kmm

---
+++ test-library/MapTest.cpp
@@ -0,0 +1,5 @@
+#include "MapTest.hpp"
+
+void MapTest::run() {
+}
+
---
+++ test-library/MapTest.hpp
@@ -0,0 +1,12 @@
+#ifndef MapTest_hpp
+#define MapTest_hpp
+
+#include "TestSuite/Test.h"
+
+class MapTest: public TestSuite::Test {
+public:
+    void run() override;
+private:
+};
+
+#endif /* MapTest_hpp */
